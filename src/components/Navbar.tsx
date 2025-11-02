64d79bec612d5a2199edce2165a365cc8303c7b5
Ondrej Kapal <okapal@gmail.com>
2025-11-02 20:17:59 +0100

    fixing icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -114,7 +114,7 @@ export default function Navbar() {
             className="inline-flex items-center justify-center w-9 h-9 rounded-full bg-black  text-background hover:opacity-90 transition"
             aria-label="Instagram"
           >
-            <img src="/public/assets/icon-instagram.png" alt="" className="w-5 h-5 pointer-events-none" />
+            <img src="/assets/icon-instagram.png" alt="" className="w-5 h-5 pointer-events-none" />
           </a>
         
           {/* E-mail */}
@@ -123,7 +123,7 @@ export default function Navbar() {
             className="inline-flex items-center justify-center w-9 h-9 rounded-full bg-black text-background hover:opacity-90 transition"
             aria-label="Napsat e-mail"
           >
-            <img src="/public/assets/icon-mail.png" alt="" className="w-5 h-5 pointer-events-none" />
+            <img src="/assets/icon-mail.png" alt="" className="w-5 h-5 pointer-events-none" />
           </a>          
 
           {/* Logo on the right side */}
